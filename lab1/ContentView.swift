4beb4187215d437055634778f24e08dc233632ff
SolomiaKutniv <23kutniv.s@nltu.lviv.ua>
2025-09-04 11:39:50 +0300

    Changes

--- lab1/ContentView.swift
+++ lab1/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("dfiklfgikfg")
+            Text("Kutniv Solomia lab1")
         }
         .padding()
     }
