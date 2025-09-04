8f63e846c955dde0ea6968b8c030f8d04dab972e
SolomiaKutniv <23kutniv.s@nltu.lviv.ua>
2025-09-04 10:30:11 +0300

    First commit

---
+++ lab1/lab1App.swift
@@ -0,0 +1,17 @@
+//
+//  lab1App.swift
+//  lab1
+//
+//  Created by ІПЗ-31/2 on 04.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct lab1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
